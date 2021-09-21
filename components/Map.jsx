3f777e29c8c2c40fdd7185ef84ed1004565da2f1
reo127 <reo127.0.0.1@gmail.com>
2021-09-21 17:30:33 +0530

    Marker added Succsfully but desine need some fix, marker image is too big

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -27,6 +27,13 @@ export default () => (
          longitudeDelta: 0.0121,
        }}
      >
+         <Marker
+         coordinate={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+         }}
+         image={require('./assets/images/location.png')}
+         />
      </MapView>
 
 );
