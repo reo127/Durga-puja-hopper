1691bcb84c6fa33fa3020664a80250f7cca4fb74
reo127 <reo127.0.0.1@gmail.com>
2021-09-22 23:57:53 +0530

    Get location in text variable

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,57 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
-const styles = StyleSheet.create({
- container: {
-   ...StyleSheet.absoluteFillObject,
-   height: 400,
-   width: 400,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
- },
- mapMarker:{
-    width:20,
-    height:20,
- },
- map: {
-   ...StyleSheet.absoluteFillObject,
- },
-});
 
-export default () => (
 
-     <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-         <Marker
-         style={styles.mapMarker}
-         coordinate={{
+function Map() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+ 
+     console.log(text)
+
+    return (
+      <>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
             latitude: 37.78825,
             longitude: -122.4324,
-         }}
-         image={require('./assets/images/location.png')}
-         title='This First'
-         description='description of 1st'
-         />
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          <Marker
+            style={styles.mapMarker}
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+            }}
+            image={require('./assets/images/location.png')}
+            title='This First'
+            description='description of 1st'
+          />
 
           <Marker
-         style={styles.mapMarker}
-         coordinate={{
-            latitude: 37.74425,
-            longitude: -122.4324,
-         }}
-         image={require('./assets/images/location.png')}
-         title='This segent'
-         description='description of 2st'
-         />
-     </MapView>
+            style={styles.mapMarker}
+            coordinate={{
+              latitude: 37.74425,
+              longitude: -122.4324,
+            }}
+            image={require('./assets/images/location.png')}
+            title='This segent'
+            description='description of 2st'
+          />
+        </MapView>
+      </>
+    )
+  }
 
-);
+  const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    mapMarker: {
+      width: 20,
+      height: 20,
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+  });
+
+
+  export default Map
